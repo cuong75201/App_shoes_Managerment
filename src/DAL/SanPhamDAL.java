43c6018c58edc45a05fb62dc3c92467766a2d59a
cuong75201 <cuong75201@gmail.com>
2025-04-09 18:41:51 +0700

    Update SanPhamDAL

--- src/DAL/SanPhamDAL.java
+++ src/DAL/SanPhamDAL.java
@@ -5,11 +5,13 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import DAL.MySQLHelper.MySQLHelpers;
 import DTO.SanPhamDTO;
+import java.util.HashMap;
+import java.util.Map;
 import javax.swing.JOptionPane;
 
 public class SanPhamDAL {
 
-    public ArrayList<SanPhamDTO> getProductList() throws SQLException {
+    public ArrayList<SanPhamDTO> getProductList() {
         ArrayList<SanPhamDTO> list = new ArrayList<>();
         MySQLHelpers helper = new MySQLHelpers();
         ResultSet result = helper.selectAllFromTable("tblsanpham");
@@ -34,10 +36,69 @@ public class SanPhamDAL {
         } catch (SQLException e) {
             JOptionPane.showMessageDialog(null, e.getMessage(), "Lỗi", JOptionPane.ERROR_MESSAGE);
         }
-                    return list;
+        return list;
     }
-    public boolean InsertProduct(SanPhamDTO sanpham){
+
+    public boolean InsertProduct(SanPhamDTO sanpham) {
         MySQLHelpers helper = new MySQLHelpers();
-        
+        Map<String, String> params = new HashMap<>();
+        params.put("TABLE", "tblsanpham");
+        params.put("FIELD", "Magiay,Tengiay,Soluong,Gia,Size,Doituongsd,Chatlieu,Maloai,Maxx,MaMau,Mathuonghieu");
+        helper.buildingQueryParam(params);
+        ArrayList<Object> values = new ArrayList<>();
+        values.add(sanpham.getStrMaGiay());
+        values.add(sanpham.getStrTenGiay());
+        values.add(sanpham.getiSoLuong());
+        values.add(sanpham.getiSize());
+        values.add(sanpham.getiGia());
+        values.add(sanpham.getiSize());
+        values.add(sanpham.getStrDoiTuongSD());
+        values.add(sanpham.getStrChatLieu());
+        values.add(sanpham.getStrMaLoai());
+        values.add(sanpham.getStrMaxx());
+        values.add(sanpham.getStrMaMau());
+        values.add(sanpham.getStrMaThuongHieu());
+        helper.closeConnect();
+
+        return helper.insertData(values);
+    }
+
+    public boolean UpdateProduct(SanPhamDTO sanpham) {
+        MySQLHelpers helper = new MySQLHelpers();
+        Map<String, String> params = new HashMap<>();
+        params.put("TABLE", "tblsanpham");
+        params.put("WHERE", "Magiay = ?");
+        helper.buildingQueryParam(params);
+        Map<String, Object> updateValues = new HashMap<>();
+        updateValues.put("Magiay", sanpham.getStrMaGiay());
+        updateValues.put("Tengiay", sanpham.getStrTenGiay());
+        updateValues.put("Soluong", sanpham.getiSoLuong());
+        updateValues.put("Gia", sanpham.getiGia());
+        updateValues.put("Size", sanpham.getiSize());
+        updateValues.put("Doituongsd", sanpham.getStrDoiTuongSD());
+        updateValues.put("Chatlieu", sanpham.getStrChatLieu());
+        updateValues.put("Maloai", sanpham.getStrMaLoai());
+        updateValues.put("Maxx", sanpham.getStrMaxx());
+        updateValues.put("MaMau", sanpham.getStrMaMau());
+        updateValues.put("Mathuonghieu", sanpham.getStrMaThuongHieu());
+
+        ArrayList<Object> conditionValue = new ArrayList<>();
+        conditionValue.add(sanpham.getStrMaGiay());
+        boolean success = helper.updateData(updateValues, conditionValue);
+        helper.closeConnect();
+        return success;
+    }
+
+    public boolean DeleteProduct(SanPhamDTO sanpham) {
+        MySQLHelpers helper=new MySQLHelpers();
+        Map<String,String> params=new HashMap<>();
+        params.put("TABLE","tblsanpham");
+        params.put("WHERE", "Magiay = ?");
+        helper.buildingQueryParam(params);
+        ArrayList<Object> values=new ArrayList<>();
+        values.add(sanpham.getStrMaGiay());
+        boolean success=helper.deleteData(values);
+                helper.closeConnect();
+        return success;
     }
 }
--- src/Utils/Config.java
+++ src/Utils/Config.java
@@ -13,6 +13,7 @@ import java.sql.SQLException;
 public class Config {
     public static String USER_NAME = "root";
     public static String PASSWORD = "";
+    
     public static String URL = "jdbc:mysql://localhost:3306/qlcuahanggiaydb";
     
     
