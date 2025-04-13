66bb095b8de28ab7dc77772703734c8322a49766
quocan529 <quocan529@gmail.com>
2025-04-13 20:36:50 +0700

    Add files via upload

---
+++ src/BLL/ChiTietHoaDonBLL.java
@@ -0,0 +1,54 @@
+package BLL;
+import DAL.ChiTietHDDAL;
+import DTO.ChiTietHDDTO;
+import java.sql.SQLException;
+import java.util.ArrayList;
+public class ChiTietHoaDonBLL {
+    private ArrayList<ChiTietHDDTO> list;
+    private ChiTietHDDAL cthoadon;
+    public ChiTietHoaDonBLL() throws SQLException{
+        list=new ArrayList<>();
+        cthoadon=new ChiTietHDDAL();
+        this.list=cthoadon.getDSChiTietHoaDon();
+}
+    public ArrayList<ChiTietHDDTO> getListChiTietHoaDon() {
+        return list;
+    }
+    public void setListChiTietHoaDon(ArrayList<ChiTietHDDTO> list){
+        this.list=list;
+    }
+    public boolean addChiTietHoaDon(ChiTietHDDTO temp){
+        if(cthoadon.themChiTietHoaDon(temp)){
+            list.add(temp);
+            return true;
+        }
+        return false;
+    }
+    public boolean deleteChiTietHoaDon(ChiTietHDDTO temp){
+        if(cthoadon.xoaChiTietHoaDon(temp))
+            for(var tmp : list)
+                if(tmp.getStrMaGiay()==temp.getStrMaGiay()&&tmp.getStrMaHD()==temp.getStrMaHD()){
+                    list.remove(tmp);
+                    return true;
+                }
+        return false;
+    }
+    public boolean updateChiTietHoaDon(ChiTietHDDTO temp){
+        if(cthoadon.suaChiTietHoaDon(temp))
+            for(var tmp : list)
+                if(tmp.getStrMaGiay()==temp.getStrMaGiay()&&tmp.getStrMaHD()==temp.getStrMaHD()){
+                    tmp.setiGiaBan(temp.getiGiaBan());
+                    tmp.setiSoLuong(temp.getiSoLuong());
+                    return true;
+                }
+        return false;
+    }
+//    public static void main(String[] args) throws SQLException{
+//        ChiTietHoaDonBLL temp=new ChiTietHoaDonBLL();
+//        ChiTietHDDTO tmp=new ChiTietHDDTO("SP1", "HD003", 10, 10);
+////        temp.addChiTietHoaDon(tmp);
+////        temp.deleteChiTietHoaDon(tmp);
+//        ChiTietHDDTO tmp_2=new ChiTietHDDTO("SP1", "HD001", 3, 4);
+//        temp.updateChiTietHoaDon(tmp_2);
+//    }
+}
---
+++ src/BLL/ChiTietPhieuNhapBLL.java
@@ -0,0 +1,54 @@
+package BLL;
+import DAL.ChiTietPhieuNhapDAL;
+import DTO.ChiTietPNDTO;
+import java.sql.SQLException;
+import java.util.ArrayList;
+public class ChiTietPhieuNhapBLL {
+    private ArrayList<ChiTietPNDTO> list;
+    private ChiTietPhieuNhapDAL ctphieunhap;
+    public ChiTietPhieuNhapBLL() throws SQLException{
+        list=new ArrayList<>();
+        ctphieunhap=new ChiTietPhieuNhapDAL();
+        this.list=ctphieunhap.getDSChiTietPN();
+}
+    public ArrayList<ChiTietPNDTO> getListChiTietPhieuNhap() {
+        return list;
+    }
+    public void setListChiTietPhieuNhap(ArrayList<ChiTietPNDTO> list){
+        this.list=list;
+    }
+    public boolean addChiTietPhieuNhap(ChiTietPNDTO temp){
+        if(ctphieunhap.themChiTietPhieuNhap(temp)){
+            list.add(temp);
+            return true;
+        }
+        return false;
+    }
+    public boolean deleteChiTietPhieuNhap(ChiTietPNDTO temp){
+        if(ctphieunhap.xoaChiTietPhieuNhap(temp))
+            for(var tmp : list)
+                if(tmp.getStrMaGiay()==temp.getStrMaGiay()&&tmp.getStrMaPN()==temp.getStrMaPN()){
+                    list.remove(tmp);
+                    return true;
+                }
+        return false;
+    }
+    public boolean updateChiTietPhieuNhap(ChiTietPNDTO temp){
+        if(ctphieunhap.suaChiTietPhieuNhap(temp))
+            for(var tmp : list)
+                if(tmp.getStrMaGiay()==temp.getStrMaGiay()&&tmp.getStrMaPN()==temp.getStrMaPN()){
+                    tmp.setiGiaNhap(temp.getiGiaNhap());
+                    tmp.setiSoLuong(temp.getiSoLuong());
+                    return true;
+                }
+        return false;
+    }
+//            public static void main(String[] args) throws SQLException{
+//        ChiTietPhieuNhapBLL temp=new ChiTietPhieuNhapBLL();
+//        ChiTietPNDTO tmp=new ChiTietPNDTO("PN002", "SP4", 45, 60);
+////        temp.addChiTietPhieuNhap(tmp);
+////        temp.deleteChiTietPhieuNhap(tmp);
+//        ChiTietPNDTO tmp_2=new ChiTietPNDTO("PN001", "SP1", 8, 4);
+//        temp.updateChiTietPhieuNhap(tmp_2);
+//    }
+}
