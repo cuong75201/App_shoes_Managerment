7ddf311f818fecba538edc56e4c38daff6fdfffd
Son1890 <vovanson0104@gmail.com>
2025-04-21 21:28:34 +0700

    Update MauSac.java

--- src/GUI/ThuocTinh/ThuocTinh/MauSac.java
+++ src/GUI/ThuocTinh/ThuocTinh/MauSac.java
@@ -1,8 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package GUI.ThuoctTinh;
+package GUI.ThuocTinh;
 import BLL.MauSacBLL;
 import DTO.MauSacDTO;
 import GUI.component.CustomButton;
@@ -328,5 +324,10 @@ import javax.swing.table.TableRowSorter;
         tblmausac.setRowSorter(null);
     }
 }
+
+
+
+
+
 
 
